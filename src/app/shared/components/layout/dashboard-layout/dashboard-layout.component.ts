2c304157af33f6cdea23b22a970fc155fe581ec6
Bandit Silachai <mbandit001@yahoo.co.th>
2025-04-22 14:13:28 +0700

    🎨 improve(layout): dashboard

--- src/app/shared/components/layout/dashboard-layout/dashboard-layout.component.ts
+++ src/app/shared/components/layout/dashboard-layout/dashboard-layout.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './dashboard-layout.component.html',
   styleUrl: './dashboard-layout.component.scss',
 })
