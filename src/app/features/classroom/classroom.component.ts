54a2c3710cc1db0b67c18f0cdecdee78d28981eb
Bandit Silachai <mbandit001@yahoo.co.th>
2025-04-23 23:07:51 +0700

    ✨ feat(app): add classrooms

---
+++ src/app/features/classroom/classroom.component.ts
@@ -0,0 +1,52 @@
+import { Component, input } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import {  MatTableModule } from '@angular/material/table';
+
+interface Student {
+  id: number;
+  firstName: string;
+  lastName: string;
+}
+
+const STUDENTS_DATA: Student[] = [
+  { id: 1, firstName: 'สมชาย', lastName: 'ทองดี' },
+  { id: 2, firstName: 'ศิริพร', lastName: 'แสงจันทร์' },
+  { id: 3, firstName: 'ประยุทธ์', lastName: 'คนดีศรีสุข' },
+  { id: 4, firstName: 'ปวีณา', lastName: 'ใจงาม' },
+  { id: 5, firstName: 'วราวุธ', lastName: 'ศรีสุข' },
+  { id: 6, firstName: 'อรอนงค์', lastName: 'พูนผล' },
+  { id: 7, firstName: 'สมปอง', lastName: 'นาควิไล' },
+  { id: 8, firstName: 'ณัฐพล', lastName: 'วัฒนชัย' },
+  { id: 9, firstName: 'ธนภร', lastName: 'มีสุข' },
+  { id: 10, firstName: 'จิราพร', lastName: 'ทรัพย์เจริญ' },
+  { id: 11, firstName: 'ณัฐชา', lastName: 'วงศ์วิไล' },
+  { id: 12, firstName: 'ชาญชัย', lastName: 'ตั้งมั่น' },
+  { id: 13, firstName: 'สุชาติ', lastName: 'สืบสาย' },
+  { id: 14, firstName: 'พิมพ์ชนก', lastName: 'แสงรุ่ง' },
+  { id: 15, firstName: 'ศักดิ์ดา', lastName: 'ทองแท้' },
+  { id: 16, firstName: 'รุ่งนภา', lastName: 'สุขดี' },
+  { id: 17, firstName: 'ธีรศักดิ์', lastName: 'กำแหง' },
+  { id: 18, firstName: 'วรรณา', lastName: 'ปรีชารัตน์' },
+  { id: 19, firstName: 'นพพร', lastName: 'จันทรา' },
+  { id: 20, firstName: 'สุรีย์พร', lastName: 'สายใจ' },
+];
+
+@Component({
+  selector: 'app-classroom',
+  imports: [RouterModule, MatTableModule],
+  templateUrl: './classroom.component.html',
+  styleUrl: './classroom.component.scss',
+})
+export class ClassroomComponent {
+  readonly id = input<string>();
+
+  get idValue(): string {
+    return this.id() ?? ""; // unwrap the signal
+  }
+
+
+  displayedColumns: string[] = ['No', 'FirstName', 'LastName'];
+  dataSource = STUDENTS_DATA;
+  students = STUDENTS_DATA;
+}
